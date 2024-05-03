c6e458e93d7c485dbf24b3dede2f04736c7bfa81
Maksym Kondratiuk <maksim.kondratyuk@gmail.com>
2024-05-03 08:30:55 +0200

    adjust clear

--- scripts/clear.js
+++ scripts/clear.js
@@ -4,23 +4,28 @@ const path = require("path");
 const src_path = path.join(__dirname, "..", "src");
 
 try {
-    fs.readdirSync(src_path).
-        filter(f => {
-            if (f === 'solutions') {
-                console.log("found", f);
-                return true;
-            }
-            console.log("ignoring", f);
-            return false;
-        }).
-        forEach(f => {
-            const file = path.join(src_path, f);
-            console.log("deleting", file);
-            fs.rmSync(file, {
-                recursive: true,
-                force: true,
-            });
-        });
+	const testsFile = path.join(__dirname, "..", "jest.tests.json");
+	if (fs.existsSync(testsFile)) {
+		fs.unlinkSync(testsFile);
+	}
+
+	fs.readdirSync(src_path).
+		filter(f => {
+			if (f === 'solutions') {
+				console.log("found", f);
+				return true;
+			}
+			console.log("ignoring", f);
+			return false;
+		}).
+		forEach(f => {
+			const file = path.join(src_path, f);
+			console.log("deleting", file);
+			fs.rmSync(file, {
+				recursive: true,
+				force: true,
+			});
+		});
 } catch (e) { console.log(e); }
 
 
