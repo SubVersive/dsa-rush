9c46eca3bf9a6146662f2e4a5bf06f0d1f4a06ae
Maksym Kondratiuk <maksim.kondratyuk@gmail.com>
2024-05-06 11:09:04 +0200

    Improve practice generation

--- scripts/practice.js
+++ scripts/practice.js
@@ -3,11 +3,12 @@ const path = require("path");
 const config = require("../ligma.config");
 const dsa = require("./dsa");
 
+const mode = process.argv[2];
+
 const src_path = path.join(__dirname, "..", "src");
 
 const solutions = `solutions`;
 const solutions_path = path.join(src_path, solutions);
-const relative_solutions_path = path.relative(process.cwd(), solutions_path);
 try { fs.unlinkSync(solutions_path); } catch (e) { }
 try { fs.mkdirSync(solutions_path); } catch (e) { }
 
@@ -47,21 +48,49 @@ function create_function(name, item) {
 }`);
 }
 
-const rnd = Math.floor(Math.random() * (config.practice.length - 1));
-const ds = config.practice[rnd];
-console.log(`Selected ${ds} for practice`);
-fs.writeFileSync(path.join(__dirname, "..", "jest.tests.json"), JSON.stringify([ds], null, 2));
-const item = dsa[ds];
-if (!item) {
-	throw new Error(`algorithm ${ds} could not be found`);
-}
-if (item.type === "class") {
-	create_class(ds, item);
-} else {
-	create_function(ds, item);
+function shuffle(array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
+    }
+    return array;
 }
 
+let problems;
+
+if (!mode || !Number.isNaN(parseInt(mode, 10))) {
+    const count = !mode ? 1 : parseInt(mode, 10);
+    console.log({ count });
+    problems = shuffle([...config.practice]).slice(0, count);
+} else {
+    problems = config.practice.filter((p) => p.toLowerCase().includes(mode.toLowerCase()));
+}
+
+problems.forEach((problem) => {
+    console.log(`Selected ${problem} for practice`);
+    const item = dsa[problem];
+    if (!item) {
+        throw new Error(`algorithm ${problem} could not be found`);
+    }
+    if (item.type === "class") {
+        create_class(problem, item);
+    } else {
+        create_function(problem, item);
+    }
+});
+fs.writeFileSync(path.join(__dirname, "..", "jest.tests.json"), JSON.stringify(problems, null, 2));
+
 const align = require("./align-configs");
-align.jest(solutions, ds);
+align.jest(solutions);
 align.ts_config(solutions);
 
