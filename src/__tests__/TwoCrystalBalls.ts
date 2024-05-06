7e6c0df9aab8a05cc59bfa15ea000b4f8bcda6c3
Maksym Kondratiuk <maksim.kondratyuk@gmail.com>
2024-05-06 12:31:10 +0200

    Improve crystal ball

--- src/__tests__/TwoCrystalBalls.ts
+++ src/__tests__/TwoCrystalBalls.ts
@@ -1,13 +1,31 @@
 import two_crystal_balls from "@code/TwoCrystalBalls";
 
-test("two crystal balls", function () {
-  let idx = Math.floor(Math.random() * 10000);
-  const data = new Array(10000).fill(false);
+describe('Two crystal balls', () => {
+  it('should work for empty array', () => {
+    expect(two_crystal_balls([])).toEqual(-1);
+  });
 
-  for (let i = idx; i < 10000; ++i) {
-    data[i] = true;
-  }
+  it('should work for last item', () => {
+    expect(two_crystal_balls([false, false, false, false, false, true])).toEqual(5);
+    expect(two_crystal_balls([false, false, false, false, false, false, true])).toEqual(6);
+  });
 
-  expect(two_crystal_balls(data)).toEqual(idx);
-  expect(two_crystal_balls(new Array(821).fill(false))).toEqual(-1);
+  it('should work for randomly generated array', () => {
+    const idx = Math.floor(Math.random() * 10000);
+    const data = new Array(10000).fill(false);
+
+    for (let i = idx; i < 10000; ++i) {
+      data[i] = true;
+    }
+
+    expect(two_crystal_balls(data)).toEqual(idx);
+  });
+
+  it('should work for all false case', () => {
+    expect(two_crystal_balls(new Array(821).fill(false))).toEqual(-1);
+  })
+
+  it('should work for all true case', () => {
+    expect(two_crystal_balls(new Array(821).fill(true))).toEqual(0);
+  })
 });
